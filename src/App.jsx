076e35a026e572db6c0e3f64996f27fc986c3a24
Xavi002 <diogomartins200214@gmail.com>
2020-09-07 18:27:53 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Global from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Global />
+
+    </div>
+  );
+}
+
+export default App;
