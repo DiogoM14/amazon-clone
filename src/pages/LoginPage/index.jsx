cf24053477f641b48500a01f6f4dabe37ad84a5b
Xavi002 <diogomartins200214@gmail.com>
2020-09-10 15:05:22 +0100

    UPDATED: styling login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import { Container } from "./styles";
+import { Container, LoginContainer } from "./styles";
+
+import Logo from "../../assets/login-logo.webp";
 
 function Index() {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="Amazon Logo" />
+      </Link>
+
+      <LoginContainer>
+        <h1>Sing-in</h1>
+
+        <form action="">
+          <h5>E-mail</h5>
+          <input type="text" />
+
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+
+          <p>
+            This application is the Amazon Clone, only developed for studies, it
+            is a warning that we are not responsible for any purchase made on
+            this test site!
+          </p>
+
+          <button className="login__registerButton">
+            Create your Amazon account
+          </button>
+        </form>
+      </LoginContainer>
+    </Container>
   );
 }
 
--- src/pages/LoginPage/styles.js
+++ src/pages/LoginPage/styles.js
@@ -1,3 +1,75 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    margin: 20px;
+  }
+
+  img {
+    object-fit: contain;
+    width: 100px;
+    margin: 0 auto;
+  }
+`;
+
+export const LoginContainer = styled.div`
+  width: 300px;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+  padding: 20px;
+
+  h1 {
+    font-weight: 500;
+    margin-bottom: 20px;
+  }
+
+  form {
+    h5 {
+      margin-bottom: 5px;
+    }
+
+    input {
+      width: 98%;
+      height: 30px;
+      margin-bottom: 10px;
+      background: white;
+      border: 1px solid;
+      border-radius: 4px;
+    }
+  }
+
+  p {
+    margin-top: 15px;
+    font-size: 12px;
+  }
+
+  .login__signInButton {
+    width: 100%;
+    height: 30px;
+    background: #f0c14b;
+    border: 1px solid;
+    margin-top: 10px;
+    border-color: #a88734 #9c7e31 #836a29;
+    color: #111;
+    border-radius: 4px;
+  }
+
+  .login__registerButton {
+    width: 100%;
+    height: 30px;
+    border: 1px solid;
+    margin-top: 10px;
+    border-color: darkgray;
+    border-radius: 4px;
+  }
+`;
