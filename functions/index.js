351740143f0ec3bc13a114300706cf037876134a
Xavi002 <diogomartins200214@gmail.com>
2020-09-14 15:16:12 +0100

    UPDATE: setting up express server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HQCBLLoTsVSh7HpezD2JBPtCfrctc3usWIGWskiRKGpVveq3XGYuYm2M6d0e9CPdZsr1CjF6owvb51oIg1G1Mqt00DDzB9eS0"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+// Listen Command
+exports.api = functions.https.onRequest(app);
