32842b3bc944d471739908366287cb9797a2a8d0
Xavi002 <diogomartins200214@gmail.com>
2020-09-16 21:25:52 +0100

    ADDED: pushing orders data into orders page

--- src/pages/OrdersPage/index.jsx
+++ src/pages/OrdersPage/index.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "../../components/Header/index.jsx";
+import { db } from "../../services/firebase.js";
+import { useStateValue } from "../../store/StateProvider.jsx";
 
 import { Container } from "./styles";
 
-export default function index() {
+export default function Index() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
   return (
     <>
       <Header />
