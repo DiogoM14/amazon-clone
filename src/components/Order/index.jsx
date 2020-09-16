dca14ea3064eae36b25e4119a80967685f3c4b8d
Xavi002 <diogomartins200214@gmail.com>
2020-09-16 21:47:37 +0100

    UPDATED: displaying order/ currency format

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from "moment";
+import CurrencyFormat from "react-currency-format";
 
 import { Container } from "./styles";
 import CheckoutProduct from "../CheckoutProduct";
@@ -22,6 +23,16 @@ function Order({ order }) {
           rating={item.rating}
         />
       ))}
+      <CurrencyFormat
+        renderText={(value) => (
+          <h3 className="order__total">Order Total: {value}</h3>
+        )}
+        decimalScale={2}
+        value={order.data.amount / 100}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
     </Container>
   );
 }
